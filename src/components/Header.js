3388864eb4206375e448cdbd9254c530270af27f
Yanik dos Santos <yrsvntos@gmail.com>
2025-07-01 19:09:03 +0100

    Criar a pasta de componentes reutilizavéis

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+export default function Header(){
+    return(
+        <div className="menu-nav">
+            <div className="logo">
+                <img/>
+            </div>
+            <div className="menu">
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Menu</li>
+                        <li>Reservation</li>
+                        <li>Order Online</li>
+                        <li>Login</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
+}
